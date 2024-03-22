616b9cca9cc7f250b8182f9ce7401dfb9532b8ae
Ryan Barry <ryan@ryan-ubuntu20.04-desktop>
2024-03-22 00:04:07 -0400

    add joystick emulator

---
+++ src/notspot_joystick/scripts/JoystickEmulator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# Author: Ryan Barry
+
+import rospy
+from math import fabs
+from numpy import array_equal
+
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+class EmulateJoystick(object):
+    def __init__(self, rate):
+        rospy.init_node("joystick_emulator")
+        rospy.Subscriber("gait", String, self.set_gait)
+        rospy.Subscriber("velocity", Twist, self.set_velocity)
+        self.publisher = rospy.Publisher("notspot_joy/joy_ramped", Joy, queue_size=10)
+        self._gait = "stand"
+        self._velocity = Twist()
+        self.rate = rospy.Rate(rate)
+        self.joy = Joy()
+        self.joy.axes = [0.,0.,0.,0.,0.,0.,0.,0.]
+        self.joy.buttons = [0,0,0,0,0,0,0,0,0,0,0]
+        
+        
+    def set_gait(self, gait):
+        self._gait = gait.data
+        self.joy.buttons = [0,0,0,0,0,0,0,0,0,0,0]  # Reset all buttons
+        
+        # Update buttons based on gait
+        if self._gait == "rest":
+            self.joy.buttons[0] = 1
+        elif self._gait == "trot":
+            self.joy.buttons[1] = 1
+        elif self._gait == "crawl":
+            self.joy.buttons[2] = 1
+        elif self._gait == "stand":
+            self.joy.buttons[3] = 1
+        else:
+            rospy.logwarn("Unknown gait: %s" % self._gait)
+            
+    def set_velocity(self, velocity):
+        self._velocity = velocity
+        
+        self.joy.axes[4] = self._velocity.linear.x
+        self.joy.axes[3] = self._velocity.linear.y
+        self.joy.axes[0] = self._velocity.angular.z
+        
+            
+            
+    def run(self):
+        while not rospy.is_shutdown():
+            self.publisher.publish(self.joy)
+            self.rate.sleep()
+        
+
+
+if __name__ == "__main__":
+    joystick = EmulateJoystick(10)
+    joystick.run()
+    #joystick = PS4_controller(rate = 30)
+    #joystick.run()
